09e2883cb84f6ae6631a4ae7ccdbf79e08c8da83
Kevin Attfield <k.attfield@gmail.com>
2014-02-02 19:44:06 -0800

    remove images from grunt-rev

--- Gruntfile.js
+++ Gruntfile.js
@@ -167,7 +167,6 @@ module.exports = function (grunt) {
                     src: [
                         '<%= yeoman.dist %>/scripts/{,*/}*.js',
                         '<%= yeoman.dist %>/styles/{,*/}*.css',
-                        '<%= yeoman.dist %>/images/{,*/}*.{gif,jpeg,jpg,png,webp}',
                         '<%= yeoman.dist %>/styles/fonts/{,*/}*.*'
                     ]
                 }
