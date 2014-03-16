29d9628aa2409139aaa48dcdef593b6f3680f481
Kevin Attfield <k.attfield@gmail.com>
2014-03-16 13:57:09 -0700

    transition main menu only for transition to and from landing page

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -5,17 +5,24 @@ $.fn.fullpage({
     scrollingSpeed: 800,
     easing: 'easeInOutCubic',
     onLeave: function(leaving, direction) {
-        var incoming = leaving + (direction === 'down'? 1: -1);
-        $('#page-header').fadeOut();
+        var wasClickNavigation = function() {
+            return $('body').is('[class*="entering-"]')
+        }
+        var entering = leaving + (direction === 'down'? 1: -1);
+        $('body').removeClass('section-1 section-2 section-3 section-4 section-5 leaving-');
+        $('body').addClass('leaving-' + leaving);
         $('.social-links').hide();
 
         if(leaving === 1) {
             $('#derp-head').data('changeImage', true);
         }
-        $('body').removeClass('section-1 section-2 section-3 section-4 section-5');
-        $('body').addClass('section-' + incoming);
+        if(!wasClickNavigation()) {
+            $('body').addClass('entering-' + entering);
+        }
     },
     afterLoad: function(anchorLink, index, slideIndex, direction) {
+        $('body').removeClass('leaving-1 leaving-2 leaving-3 leaving-4 leaving-5 entering-1 entering-2 entering-3 entering-4 entering-5');
+        $('body').addClass('section-' + index);
         $('#page-header').fadeIn();
         $('#page-header')[index === 1? 'addClass': 'removeClass']('navbar-vertical');
         $('#page-header')[index === 1? 'removeClass': 'addClass']('navbar-horizontal');
@@ -29,6 +36,13 @@ $.fn.fullpage({
             $('#derp-head').data('changeImage', false);
         }
     }
+});
+
+$('body').on('click', '[data-target]', function(e) {
+    console.log('click')
+    var origin = $('body').prop('class').match(/section-(\d)/)[1];
+    var target = $(e.target).data('target');
+    $('body').addClass('leaving-' + origin + ' entering-' + target);
 });
 
 $('body').addClass('loaded');
